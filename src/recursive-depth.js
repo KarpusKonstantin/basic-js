25992f738c1803d7249965b110e0862940a16e93
KarpusKonstantin <60568968+KarpusKonstantin@users.noreply.github.com>
2020-03-06 10:46:40 +0300

    feat: update code by node 12

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,15 +2,15 @@ module.exports = class DepthCalculator {
     calculateDepth(arr) {
         let result = 1;
         let maxresult = 1;
-        console.log('Array In = ', arr, ' Result In = ', result);
+//        console.log('Array In = ', arr, ' Result In = ', result);
 
         for (let i = 0; i < arr.length; i++) {
             if (Array.isArray(arr[i])) {
                 result = this.calculateDepth(arr[i]) + 1;
                 maxresult = Math.max(maxresult, result);
-                console.log('Array = ', arr[i],' Result = ', result,' MaxResult = ', maxresult);
+//                console.log('Array = ', arr[i],' Result = ', result,' MaxResult = ', maxresult);
             } else {
-                console.log('Not array = ', arr[i], ' Current result = ', result, ' MaxResult = ', maxresult);
+//                console.log('Not array = ', arr[i], ' Current result = ', result, ' MaxResult = ', maxresult);
             }
         }
 
